8c832b82c5a724f5830df3cf069bd11c191ffc74
MartinRemi <mrmi11@yahoo.fr>
2015-12-08 10:28:26 -0500

    Added menu and multiplayer lobby

--- Source/Tutorial/Tutorial.Build.cs
+++ Source/Tutorial/Tutorial.Build.cs
@@ -6,9 +6,9 @@ public class Tutorial : ModuleRules
 {
 	public Tutorial(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystemNull" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
